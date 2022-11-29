644364ccbc98d56406ed12c07c2ac71a112216c6
Corwin Joy <corwinjoy@gmail.com>
2022-11-28 19:45:18 -0800

    Get simple cuda matrix multiplication kernel up and running.

--- Samples/MatrixMultiplyMaskedSparse/Program.cs
+++ Samples/MatrixMultiplyMaskedSparse/Program.cs
@@ -1,4 +1,6 @@
-﻿/* Problem statement:
+﻿#define MATDEBUG
+
+/* Problem statement:
  * Create a CUDA kernal for the following sparse matrix problem
  * C = P && A*B'
  * where
@@ -34,7 +36,8 @@ namespace MatrixMultiply
         /// </summary>
         static void Main()
         {
-            SmokeTest(3, 2);
+            // SmokeTest(3, 2);
+            CudaSmokeTest(200, 150);
         }
 
         #region Helper functions
@@ -88,7 +91,142 @@ namespace MatrixMultiply
             Console.WriteLine("***************************************************************\n");
         }  
 
+
+        /*
+        * Compute
+        *
+        * C = P && A*B' using matrix operations
+        *
+        * where
+        * A = n x k, dense matrix
+        * B = k x k, sparse matrix
+        * P = n x k, dense matrix of 1s and 0s indicating if we care about an entry in the product A*B'
+        */
+        static void CudaSmokeTest(int n, int k)
+        {
+            int band_width = 100;
+            float[,] A = CreateSequentialMatrix(n, k);
+            float[,] B = CreateBandedSequentialMatrix(k, k, band_width);
+            float[,] P = CreateIndentityMatrix(n, k);
+
+            var sw = new Stopwatch();
+            
+            sw.Restart();
+            SparseMatrix sB = new SparseMatrix(B);
+            CondensedProductRows cPA = new CondensedProductRows(P, sB, A);
+            sw.Stop();
+            Console.WriteLine("Finished constructing matricies.");
+            Console.WriteLine($"Matrix parsing took: {sw.ElapsedMilliseconds}ms");  
+
+
+
+            #if MATDEBUG
+            // Debug check
+            sw.Restart();
+            float[,] Bt = MatrixTranspose(B);
+            float[,] ABt = MatrixMultiplyNaive(A, Bt);
+            float[,] P_and_ABt = MatrixMask(ABt, P);
+            sw.Stop();
+            Console.WriteLine("Finished debug check.");
+            Console.WriteLine($"Naive multiplication takes: {sw.ElapsedMilliseconds}ms"); 
+            #endif
+            
+
+            float[,] PABt = new float[n, k];
+
+            // Accelerated implementations
+            using var context = Context.CreateDefault();
+            foreach (var device in context)
+            {
+                using var accelerator = device.CreateAccelerator(context);
+                sw.Restart();
+                CondensedMatrixMultiplication(accelerator, cPA, sB, PABt);
+                sw.Stop();
+                #if MATDEBUG
+                Debug.Assert(MatrixEqual(PABt, P_and_ABt));
+                #endif
+                
+                //Console.WriteLine("PABt:"); PrintMatrix(PABt);
+                //Console.WriteLine("P_and_ABt:"); PrintMatrix(P_and_ABt);
+                Console.WriteLine($"- Accelerated implementation on {accelerator}: {sw.ElapsedMilliseconds}ms");  
+            }         
+        }
+
         #endregion   
+
+
+        #region Accelerated algorithm
+
+        // 
+        /// <summary>
+        /// Compute PABt = PA * B'
+        /// </summary>
+        /// <param name="accelerator">The Accelerator to run the multiplication on</param>
+        /// <param name="PA">A condensed version of the matrix (P && A) </param>
+        /// <param name="B">A sparse matrix Bx</param>
+        /// <param name="PABt">The dense matrix to return values into, PA * B'</param>
+        static void CondensedMatrixMultiplication(Accelerator accelerator, CondensedProductRows PA, SparseMatrix B, 
+            float [,] PABt) {
+            var kernel = accelerator.LoadAutoGroupedStreamKernel<
+                Index1D,
+                ArrayView1D<int, Stride1D.Dense>,
+                ArrayView2D<float, Stride2D.DenseX>,
+                ArrayView2D<float, Stride2D.DenseX>,
+                ArrayView1D<float, Stride1D.Dense>>(
+                    AcceleratedDotProductKernel
+                );
+
+            int a_row = PA.m_data.GetLength(0);
+            int max_col = PA.m_data.GetLength(1);
+            int b_row = B.m_edge_weights.GetLength(0);
+        
+            using var col_idx = accelerator.Allocate1D(PA.m_col_idx);
+            using var rows = accelerator.Allocate2DDenseX<float>(new Index2D(a_row, max_col));
+            using var cols = accelerator.Allocate2DDenseX<float>(new Index2D(b_row, max_col));
+            using var dotsum = accelerator.Allocate1D<float>(a_row);
+
+            col_idx.CopyFromCPU(PA.m_col_idx); // do we need this line?
+            rows.CopyFromCPU(PA.m_data);
+            cols.CopyFromCPU(B.m_edge_weights);
+
+            kernel(dotsum.Extent.ToIntIndex(), col_idx.View, rows.View, cols.View, dotsum.View);
+
+            // Copy result to empty dense output matrix
+            float[] h_dotsum = dotsum.GetAsArray1D();
+            for(int i=0; i < a_row; ++i) 
+            {
+                int r = PA.m_row_idx[i];
+                int c = PA.m_col_idx[i];
+                PABt[r, c] = h_dotsum[i];
+            }
+        }
+
+
+
+        // Form dot product between rows and cols
+        // Matching rows to columns via col_idx
+        // Return result in dotsum.
+        //   where dotsum[x] = rows[x, ] * cols[col_idx[x], ]
+        static void AcceleratedDotProductKernel(
+            Index1D index,
+            ArrayView1D<int, Stride1D.Dense> col_idx,
+            ArrayView2D<float, Stride2D.DenseX> rows,
+            ArrayView2D<float, Stride2D.DenseX> cols,
+            ArrayView1D<float, Stride1D.Dense> dotsum)
+        {
+            int row = index.X;
+            int col = col_idx[row];
+            float sum = 0.0f;
+
+            for (var i = 0; i < rows.IntExtent.Y; i++)
+                sum += rows[new Index2D(row, i)] * cols[new Index2D(col, i)];
+
+            dotsum[index] = sum;
+        }
+
+ 
+
+        #endregion
 
     } // end class Program
 } // end namespace MatrixMultiply
